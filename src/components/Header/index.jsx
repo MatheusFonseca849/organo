b0ef074d89aef482378943651d7f551c61947232
MatheusFonseca849 <matheusfonseca849@gmail.com>
2023-11-04 10:00:51 -0300

    initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+export const Header = () => {
+
+
+    return (
+
+        <header>
+            <div className="firstColumn">
+                <img src="./src/assets/logo.svg" alt="organo" />
+                <div className="headerTitle">
+                    <h1>Pessoas e times</h1>
+                    <span>organizados em um só lugar</span>
+                </div>
+            </div>
+            <div className="secondColumn">
+                <img src="./src/assets/headerImg.svg" alt="header image" />
+            </div>
+        </header>
+
+    )
+
+
+}
